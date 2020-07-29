4667da9fa6c96ca81521d747270a2b73b0b599be
Piotr Larysz <piotr.larysz@dazn.com>
2020-07-29 20:40:26 +0200

    generating few boxes in response

--- genetic-reactive-web/src/main/java/genetic/web/controller/PackerController.kt
+++ genetic-reactive-web/src/main/java/genetic/web/controller/PackerController.kt
@@ -1,44 +1,77 @@
 package genetic.web.controller
 
-import genetic.packer.dto.request.ParamsDto
-import genetic.packer.dto.request.RequestDto
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
-import java.util.concurrent.TimeUnit
-import javax.validation.Valid
 
 @RestController
 @SpringBootApplication
 class PackerController {
 
-  @GetMapping("string")
-  fun string() = "abc huehue"
-
-  @GetMapping("monoString")
-  fun monoString() = Mono.just("abc huehue")
-
-  @GetMapping("fluxStrings")
-  fun fluxStrings() = Flux.just("abc", "huehue")
-
-  @GetMapping("fluxStringsSSE", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-  fun fluxStringsSSE() = Flux.just("abc", "huehue")
-
   @GetMapping("fluxBoxes", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-  fun fluxBoxes() = Flux.generate<Box, Int>({ 1 }, { state, sink ->
-    sink.next(Box(state, state * 2))
-    state + 1
-  })
-      .take(10)
+  fun fluxBoxes(/*@RequestBody packerRequest: PackerRequest*/) =
+      Flux.generate<PackerResponse, Int>({ 1 }, { state, sink ->
+        sink.next(
+            PackerResponse(
+                boxes = sequenceOf(
+                    PositionedBox(width = 80, depth = 120, x = 0, y = 0),
+                    PositionedBox(width = 80, depth = 120, x = 80, y = 0),
+                    PositionedBox(width = 80, depth = 120, x = 0, y = 120),
+                    PositionedBox(width = 80, depth = 120, x = 80, y = 120),
+                    PositionedBox(width = 80, depth = 120, x = 160, y = 0),
+                    PositionedBox(width = 80, depth = 120, x = 160, y = 120)
+                )
+            )
+        )
+        1
+      })
+          .take(1)
 }
 
-data class Box(val width: Int, val depth: Int)
+data class PackerRequest(
+    val container: Container,
+    val boxes: Sequence<Box>
+)
+
+data class PackerResponse(
+    val boxes: Sequence<PositionedBox>
+)
+
+interface Size {
+  val width: Int
+  val depth: Int
+}
+
+interface Position {
+  val x: Int
+  val y: Int
+}
+
+interface Rotated {
+  val rotated: Boolean
+}
+
+data class Container(
+    override val width: Int,
+    override val depth: Int
+) : Size
+
+data class Box(
+    override val width: Int,
+    override val depth: Int
+) : Size
+
+data class PositionedBox(
+    override val width: Int,
+    override val depth: Int,
+    override val x: Int,
+    override val y: Int,
+    override val rotated: Boolean = false
+) : Size, Position, Rotated
 
 fun main(args: Array<String>) {
   runApplication<PackerController>(*args)
